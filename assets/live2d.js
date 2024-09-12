91dff93e2e567f41a4d0625dcf8c6744342fc32c
Ia-UAn <3355409441@qq.com>
2024-09-12 12:59:53 +0800

    Update live2d.js

--- assets/live2d.js
+++ assets/live2d.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (window.innerWidth >= 1250) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://blog.liyifan.xyz/live2d/autoload.js';
+        script.src = 'https://raw.githubusercontent.com/Ia-UAn/Ia-UAn.github.io/main/live2d/autoload.js';
         document.body.appendChild(script);
     }
     
